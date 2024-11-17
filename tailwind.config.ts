12516e1ca0b96c451a97ed0500d10c0819c58eee
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-11-16 21:16:35 -0600

    Added home page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,50 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        inter: ["var(--font-inter)", "sans-serif"],
+        nacelle: ["var(--font-nacelle)", "sans-serif"],
+      },
+      fontSize: {
+        xs: ["0.8125rem", { lineHeight: "1.5384" }],
+        sm: ["0.875rem", { lineHeight: "1.5715" }],
+        base: [
+          "0.9375rem",
+          { lineHeight: "1.5333", letterSpacing: "-0.0125em" },
+        ],
+        lg: ["1.125rem", { lineHeight: "1.5", letterSpacing: "-0.0125em" }],
+        xl: ["1.25rem", { lineHeight: "1.5", letterSpacing: "-0.0125em" }],
+        "2xl": ["1.5rem", { lineHeight: "1.415", letterSpacing: "-0.0268em" }],
+        "3xl": [
+          "1.75rem",
+          { lineHeight: "1.3571", letterSpacing: "-0.0268em" },
+        ],
+        "4xl": ["2.5rem", { lineHeight: "1.1", letterSpacing: "-0.0268em" }],
+        "5xl": ["3.5rem", { lineHeight: "1", letterSpacing: "-0.0268em" }],
+        "6xl": ["4rem", { lineHeight: "1", letterSpacing: "-0.0268em" }],
+        "7xl": ["4.5rem", { lineHeight: "1", letterSpacing: "-0.0268em" }],
+      },
+      animation: {
+        shine: "shine 5s ease-in-out 500ms infinite",
+      },
+      keyframes: {
+        shine: {
+          "0%": { top: "0", transform: "scaleY(5)", opacity: "0" },
+          "10%": { opacity: ".8" },
+          "20%": { top: "100%", transform: "scaleY(10)", opacity: "0" },
+          "100%": { top: "100%", transform: "scaleY(1)", opacity: "0" },
+        },
+        gradient: {
+          to: { "background-position": "200% center" },
+        },
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require("@tailwindcss/forms")],
+};
