b07075de57ec165c2841a2675777c0caf3bfea42
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-11-17 11:01:41 -0600

    Fixed cheque system

--- app/api/cheque/route.ts
+++ app/api/cheque/route.ts
@@ -1,6 +1,8 @@
 import { writeFile, mkdir, unlink, rmdir } from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
 import ollama from "ollama";
+import { createClient } from "@/utils/supabase/supabase";
+import supabase2 from "@/utils/supabase/supersupabase";
 
 export async function POST(request: NextRequest) {
   const data = await request.formData();
@@ -34,13 +36,106 @@ export async function POST(request: NextRequest) {
   try {
     parsedData = JSON.parse(res);
   } catch {
-    return NextResponse.json({ success: false, error: "Failed to parse JSON" });
+    return Response.json(
+      { error: "Failed to parse JSON" },
+      {
+        status: 401,
+      }
+    );
   }
 
-  console.log(parsedData);
+  console.log(parsedData.data);
 
   await unlink(filePath);
-  await rmdir(dirPath);
+  await rmdir(dirPath, { recursive: true });
 
-  return NextResponse.json({ success: true, data: parsedData });
+  const supabase = await createClient();
+  const { data: user } = await supabase.auth.getUser();
+
+  if (user.user === null) {
+    return Response.json(
+      { error: "Not Logged in!" },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const name = parsedData.data.name;
+  const amount = parsedData.data.amount;
+
+  const UUID = "0cad46c7-8e27-4e50-9bed-73232744483b";
+
+  const { data: temp3, error: error901 } = await supabase2
+    .from("users")
+    .select("name")
+    .eq("id", user.user.id);
+
+  if (error901 || temp3.length === 0) {
+    return new Response(
+      JSON.stringify({
+        message: "Error in fetching receiver's name",
+        error: "Error in fetching receiver's name",
+      })
+    );
+  }
+
+  const receiverName = temp3[0].name;
+
+  const { error: error3 } = await supabase2.from("transactions").insert([
+    {
+      sender: UUID,
+      receiver: user.user.id,
+      amount,
+      success: true,
+      sender_name: name,
+      receiver_name: receiverName,
+    },
+  ]);
+
+  if (error3) {
+    return Response.json(
+      { error: error3.message },
+      {
+        status: 500,
+      }
+    );
+  }
+
+  let { data: amtReceiver, error: error22 } = await supabase2
+    .from("users")
+    .select("amount")
+    .eq("id", user.user.id);
+
+  if (error22 || !amtReceiver || amtReceiver.length === 0) {
+    return new Response(
+      JSON.stringify({
+        message: "Error in fetching receiver's amount",
+        error: "Error in fetching receiver's amount",
+      })
+    );
+  }
+
+  amtReceiver = amtReceiver[0].amount;
+
+  const { error } = await supabase2
+    .from("users")
+    .update({ amount: amtReceiver + amount })
+    .eq("id", user.user.id);
+
+  if (error) {
+    return Response.json(
+      { error: error.message },
+      {
+        status: 500,
+      }
+    );
+  }
+
+  return Response.json(
+    { message: "Transaction Successful" },
+    {
+      status: 200,
+    }
+  );
 }
