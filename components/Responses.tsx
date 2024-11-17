dc43b7224f8b794a042369d81946f83f7fb29c1b
Saadhya Vijayvargiya <97754092+saadhya0102@users.noreply.github.com>
2024-11-17 09:23:07 -0600

    removed comments

--- components/Responses.tsx
+++ components/Responses.tsx
@@ -2,77 +2,33 @@
 
 import { useState } from "react";
 import useMasonry from "@/utils/useMasonry";
-import Image, { StaticImageData } from "next/image";
-import TestimonialImg01 from "@/public/images/testimonial-01.jpg";
-import TestimonialImg02 from "@/public/images/testimonial-02.jpg";
-import TestimonialImg03 from "@/public/images/testimonial-03.jpg";
-import TestimonialImg04 from "@/public/images/testimonial-04.jpg";
-import TestimonialImg05 from "@/public/images/testimonial-05.jpg";
-import TestimonialImg06 from "@/public/images/testimonial-06.jpg";
-import TestimonialImg07 from "@/public/images/testimonial-07.jpg";
-import TestimonialImg08 from "@/public/images/testimonial-08.jpg";
-import TestimonialImg09 from "@/public/images/testimonial-09.jpg";
-import ClientImg01 from "@/public/images/client-logo-01.svg";
-import ClientImg02 from "@/public/images/client-logo-02.svg";
-import ClientImg03 from "@/public/images/client-logo-03.svg";
-import ClientImg04 from "@/public/images/client-logo-04.svg";
-import ClientImg05 from "@/public/images/client-logo-05.svg";
-import ClientImg06 from "@/public/images/client-logo-06.svg";
-import ClientImg07 from "@/public/images/client-logo-07.svg";
-import ClientImg08 from "@/public/images/client-logo-08.svg";
-import ClientImg09 from "@/public/images/client-logo-09.svg";
-
 const testimonials = [
   {
-    //img: TestimonialImg01,
-    //clientImg: ClientImg01,
-    //name: "MaKayla P.",
-    //company: "Disney",
     content:
       "Through the use of 2 factor authentication, we ensure that your account remains secure against misuse or fraud. This also makes disabling your account easier in case issues arise.",
     categories: [1, 2, 4],
   },
   {
-    //img: TestimonialImg02,
-    //clientImg: ClientImg02,
-    //name: "Andrew K.",
-    //company: "Samsung",
     content:
       "You can effectively send and recieve payments with nothing but the other party's User ID. This also means that only the buyer needs to have a device, making this more accessible for various kinds of vendors.",
     categories: [1, 3, 5],
   },
   {
-    //img: TestimonialImg03,
-    //clientImg: ClientImg03,
-    //name: "Lucy D.",
-    //company: "Rio",
     content:
       "Prior to sending a payment, you can book it, assuring your vendor that they will recieve the payment, prior to receiving the service.",
     categories: [1, 3],
   },
   {
-    //img: TestimonialImg04,
-    //clientImg: ClientImg04,
-    //name: "Pavel M.",
-    //company: "Canon",
     content:
       "We take no cut of transactions, deriving all profit from a small extraction fee based solely on a percentage of a withdrawal, with smaller percent cuts for smaller withdrawals.",
     categories: [1, 4, 5],
   },
   {
-    //img: TestimonialImg05,
-    //clientImg: ClientImg05,
-    //name: "Miriam E.",
-    //company: "Cadbury",
     content:
       "We verify government IDs prior to account registration to further enhance security, a feature other digital wallets often lack. This combines the security of a physical bank and the accessibly of a digital one.",
     categories: [1, 2],
   },
   {
-    //img: TestimonialImg06,
-    //clientImg: ClientImg06,
-    //name: "Eloise V.",
-    //company: "Maffell",
     content:
       "It only takes a few clicks to handle payments, so large quantities of payments on a daily basis are easy to handle, especially as there is no transaction fee, ensuring that it's an effective method of payment compared to standard credit cards.",
     categories: [1, 3],
@@ -209,10 +165,6 @@ export function Testimonial({
   children,
 }: {
   testimonial: {
-    //img: StaticImageData;
-    //clientImg: StaticImageData;
-    //name: string;
-    //company: string;
     content: string;
     categories: number[];
   };
@@ -231,16 +183,7 @@ export function Testimonial({
           {children}
         </p>
         <div className="flex items-center gap-3">
-          {/*<Image
-            className="inline-flex shrink-0 rounded-full"
-            src={testimonial.img}
-            width={36}
-            height={36}
-            alt={testimonial.name}
-          />*/}
           <div className="text-sm font-medium text-gray-200">
-            {/*<span>{testimonial.name}</span>
-            <span className="text-gray-700"> - </span>*/}
             <a
               className="text-indigo-200/65 transition-colors hover:text-indigo-500"
               href="#0"
